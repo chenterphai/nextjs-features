471557e020b3c9bf415db455402854ca3726afd7
Chenter <chenterphai61@gmail.com>
2024-04-19 14:04:04 +0700

    Updated

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,25 @@
-export default async function Home() {
+'use client'
+import PopularServices from "@/components/Popular";
+import * as React from 'react'
+import { supabaseClient } from "@/utils/supabase/client";
+import RecentProject from "@/components/RecentProject";
+export default function Home() {
+
+  const supabase = supabaseClient()
+  const [data, setData] = React.useState<any[] | null>(null)
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const { data, error } = await supabase.from('services').select('title, image').order('id', { ascending: false }).limit(5)
+      setData(data)
+    }
+    getData()
+  }, [])
 
   return (
     <>
-
+      <PopularServices data={data} />
+      <RecentProject />
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const inter = Poppins({ subsets: ["latin"], weight: ["500"] });
+const inter = Poppins({ subsets: ["latin"], weight: ["500", "600", "700", "800", "900", "400", "300"] });
 
 export const metadata: Metadata = {
   title: "NYT Technology",
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <Navbar >
+
+          {children}
+        </Navbar>
       </body>
     </html>
   );
